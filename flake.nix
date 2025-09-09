e16256c48a4183b94c5d9e74610193a5faa12dde
Elec3137 <elec3137@proton.me>
2025-09-09 14:02:04 -0700

    add to flake with nixos-hardware
    disabled by default, but useful for many devices

--- flake.nix
+++ flake.nix
@@ -5,11 +5,19 @@
     # i.e. nixos-25.05
     # Use `nix flake update` to update the flake to the latest revision of the chosen release channel.
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # optional input: "NixOS profiles to optimize settings for different hardware"
+    # nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
   outputs = { nixpkgs, ... }: {
-      # NOTE: 'nixos' is the default hostname
-      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-        modules = [ ./configuration.nix ];
-      };
+    # NOTE: 'nixos' is the default hostname
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      modules = [
+        ./configuration.nix
+        
+        # nixos-hardware.nixosModules.YOUR_DEVICE
+        # add your device if it is listed here: https://github.com/NixOS/nixos-hardware
+      ];
     };
+  };
 }
