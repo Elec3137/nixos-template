b498004d03b5f0d7a90125ed22b9c5ebb7eeda27
Elec3137 <elec3137@proton.me>
2025-09-09 14:58:18 -0700

    add nvidia template
    disabled by default

--- configuration.nix
+++ configuration.nix
@@ -275,7 +275,42 @@
   # networking.firewall.enable = false;
 
   # leave this uncommented if you use any unfree packages/modules
+  # even the "open" Nvidia drivers have proprietary userside components: https://wiki.nixos.org/wiki/NVIDIA
   nixpkgs.config.allowUnfree = true;
+
+  # Nvidia block; uncomment only for Nvidia GPUs
+  # hardware.graphics.enable = true; # OpenGL
+  # Load nvidia driver for Xorg and Wayland
+  # services.xserver.videoDrivers = ["nvidia"];
+  # hardware.nvidia = {
+    # Modesetting is required.
+    # modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+    # of just the bare essentials.
+    # powerManagement.enable = false;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    # powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 515.43.04+
+    # open = false;
+
+    # Enable the Nvidia settings menu,
+  	# accessible via `nvidia-settings`.
+    # nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    # package = config.boot.kernelPackages.nvidiaPackages.stable;
+  # };
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
