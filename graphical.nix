cb30bf174de735550c02744de6b02ca2f331054a
Elec3137 <elec3137@proton.me>
2025-09-26 14:08:48 -0700

    avoid pre-rename fonts option

--- graphical.nix
+++ graphical.nix
@@ -149,7 +149,7 @@
       settings.addons.classicui.globalSection.Theme = "plasma";
     };
   };
-  fonts.enableDefaultFonts = true;
+  fonts.enableDefaultPackages = true;
 
   # enable waydroid, a container-based solution to android emulation
   # virtualisation.waydroid.enable = true;
