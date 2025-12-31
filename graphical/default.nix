53ac4d23d064fa98386dac6356db068ada07aaa2
Elec3137 <elec3137@proton.me>
2025-12-31 11:39:52 -0800

    move browser stuff to graphical/browsers.nix

--- graphical/default.nix
+++ graphical/default.nix
@@ -5,6 +5,7 @@
 
 {
   imports = [
+    ./browsers.nix
     # ./games.nix
   ];
 
@@ -49,8 +50,6 @@
   # services.printing.enable = true;
 
   environment.systemPackages = with pkgs; [
-    ungoogled-chromium
-
     # videos
     mpv
     kdePackages.kdenlive
@@ -78,41 +77,6 @@
     # torrenting client
     qbittorrent
   ];
-
-  programs.firefox = {
-    # enable = true;
-    package = pkgs.librewolf;
-    preferences = {
-      "privacy.resistFingerprinting" = false; # due to it forcing light theme
-      "webgl.disabled" = false;
-
-      # these should only be on by the user's request
-      "privacy.clearHistory.cookiesAndStorage" = false;
-      "privacy.clearOnShutdown.cookies" = false;
-      "privacy.clearOnShutdown_v2.cookiesAndStorage" = false; # this one seems to be the normal setting
-    };
-  };
-
-  programs.chromium = {
-    enable = true;
-
-    # this won't work until https://github.com/NixOS/nixpkgs/pull/394028
-    # package = pkgs.ungoogled-chromium;
-
-    # note: you cannot use policies to install extensions on ungoogled-chromium
-    # https://github.com/ungoogled-software/ungoogled-chromium/issues/1629
-    extensions = [
-      "cjpalhdlnbpafiamejdnhcphjbkeiagm" # ublock origin *not working?)
-      "mnjggcdmjocbbbhaepdhchncahnbgone" # sponsorblock
-      "eimadpbcbfnmbkopoojfekhnkhdbieeh" # dark reader
-      # "ocaahdebbfolfmndjeplogmgcagdmblk" # chromium web store extension for updating other extensions on chromium
-    ];
-
-    # note: this will make it impossible to change search engines while the module is enabled
-    defaultSearchProviderEnabled = true;
-    defaultSearchProviderSearchURL = "https://www.startpage.com/sp/search?query={searchTerms}";
-    defaultSearchProviderSuggestURL = "https://www.startpage.com/osuggestions?q={searchTerms}";
-  };
 
   # enable default fonts (helps with emojis, etc)
   fonts.enableDefaultPackages = true;
