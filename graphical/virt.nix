923206b65d84a419ab984828107990c89801986c
Elec3137 <elec3137@proton.me>
2025-12-31 16:22:13 -0800

    set default waydroid package to waydroid-nftables

--- graphical/virt.nix
+++ graphical/virt.nix
@@ -11,6 +11,8 @@
   
   # enable waydroid, a container-based solution to android emulation
   virtualisation.waydroid.enable = true;
+  # needed sometimes till https://github.com/NixOS/nixpkgs/pull/466473
+  virtualisation.waydroid.package = pkgs.waydroid-nftables;
 
   # use virt-manager, a qemu GUI
   # programs.virt-manager.enable = true;
